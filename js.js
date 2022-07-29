ccec25f1d42ff85e1394005986047ea23d3a4ff2
Ira Hudgin <irahudgin@gmail.com>
2022-07-29 19:33:35 -0400

    added so much stuff and forgot to commit

---
+++ js.js
@@ -0,0 +1,29 @@
+function darken(e) {
+  this.classList.add("pixelhoverblack");
+  console.log(this);
+}
+
+function resolutionChange(e) {
+  //Clears last pixel count
+  let pixel = document.querySelectorAll(".pixel");
+  pixel.forEach((pixel) => {
+    pixel.remove();
+  });
+
+  let slider = document.querySelector(`#slider`);
+  let res = slider.value;
+
+  for (let i = 0; i < res * res; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.setAttribute(
+      "style",
+      `width: ${512 / res}px; height: ${512 / res}px;`
+    );
+    pixel.addEventListener("mouseover", darken);
+    mainholder.appendChild(pixel);
+  }
+}
+
+window.onload = resolutionChange;
+slider.addEventListener("click", resolutionChange);
