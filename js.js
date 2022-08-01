405d12bcd63284f29f1828df98ba628df6b932b1
Ira Hudgin <irahudgin@gmail.com>
2022-08-01 13:43:10 -0400

    buttons working with function methods in object

--- js.js
+++ js.js
@@ -37,5 +37,75 @@ function resolutionChange(e) {
   }
 }
 
+const buttonchange = {
+  darkenmethod() {
+    let pixel = document.querySelectorAll(".pixel");
+    pixel.forEach((pixel) => {
+      pixel.remove();
+    });
+
+    let slider = document.querySelector(`#slider`);
+    let res = slider.value;
+
+    for (let i = 0; i < res * res; i++) {
+      let pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.setAttribute(
+        "style",
+        `width: ${512 / res}px; height: ${512 / res}px;`
+      );
+      pixel.addEventListener("mouseover", darken);
+      mainholder.appendChild(pixel);
+    }
+  },
+  classicmethod() {
+    let pixel = document.querySelectorAll(".pixel");
+    pixel.forEach((pixel) => {
+      pixel.remove();
+    });
+
+    let slider = document.querySelector(`#slider`);
+    let res = slider.value;
+
+    for (let i = 0; i < res * res; i++) {
+      let pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.setAttribute(
+        "style",
+        `width: ${512 / res}px; height: ${512 / res}px;`
+      );
+      pixel.addEventListener("mouseover", classic);
+      mainholder.appendChild(pixel);
+    }
+  },
+  colorsmethod() {
+    let pixel = document.querySelectorAll(".pixel");
+    pixel.forEach((pixel) => {
+      pixel.remove();
+    });
+
+    let slider = document.querySelector(`#slider`);
+    let res = slider.value;
+
+    for (let i = 0; i < res * res; i++) {
+      let pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.setAttribute(
+        "style",
+        `width: ${512 / res}px; height: ${512 / res}px;`
+      );
+      pixel.addEventListener("mouseover", colors);
+      mainholder.appendChild(pixel);
+    }
+  },
+};
+
+let darkenbutton = document.querySelector("#darken");
+darkenbutton.addEventListener("click", buttonchange.darkenmethod);
+let classicbutton = document.querySelector("#classic");
+classicbutton.addEventListener("click", buttonchange.classicmethod);
+let colorsbutton = document.querySelector("#colors");
+colorsbutton.addEventListener("click", buttonchange.colorsmethod);
+
 window.onload = resolutionChange;
 slider.addEventListener("click", resolutionChange);
